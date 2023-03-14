0c4b257521f6b6ee88ac607921cacf22e75703be
seungkwanyoo <fhefk00@gmail.com>
2023-03-14 18:00:44 +0900

    0314

---
+++ src/sec03/exam01/MyHome.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+		System.out.print("이름:둘리");
+		System.out.print("나이:24");
+		System.out.println(); //<br>역할
+		System.out.print("이름:길동");
+		System.out.print("나이:27");
+		
+
+	}//end
+
+}//class end
